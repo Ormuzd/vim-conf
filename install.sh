256d258d1766289105ce980b006d9adb99203975
Alex Yang <alex890714@gmail.com>
2013-03-01 17:57:51 +0800

    ok

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+cat /proc/version | grep "Ubuntu"
+if [ $? -eq 0 ]; then
+echo "Ubuntu"
+is_red_hat=0
+fi
+
+cat /proc/version | grep "Red Hat"
+if [ $? -eq 0 ]; then
+echo "Red Hat"
+if_red_hat=1
+fi
+
 ln -s ~/.vim/vimrc ~/.vimrc 
 ln -s ~/.vim/gitignore ~/.gitignore 
+
+if [ $is_red_hat -eq 0 ]; then
 sudo apt-get install -y pyflakes exuberant-ctags
+else
+yum --enablerepo install -y pyflakes
+yum install -y ctags-etags
+fi
+
 git config --global user.name "Alex Yang"
 git config --global user.email "alex890714@gmail.com"
--- vimrc
+++ vimrc
@@ -105,4 +105,9 @@ endf
 
 " ====== 对齐线 ======
 let g:indent_guides_guide_size=1  " 宽度
-set cc=80
+if exists('+colorcolumn')
+  set colorcolumn=80
+else
+  au BufWinEnter * let w:m2=matchadd('ErrorMsg', '\%>80v.\+', -1)
+endif
+"set cc=80
