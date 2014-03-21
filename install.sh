2ccb649e54d431626dd7be32d29f7d8f06452c6c
Alex Yang <alex890714@gmail.com>
2014-03-21 10:12:49 +0800

    Add the screenrc file.

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ fi
 
 ln -s ~/.vim/vimrc ~/.vimrc 
 ln -s ~/.vim/gitignore ~/.gitignore 
+ln -s ~/.vim/screenrc ~/.screenrc
 
 if [ $is_red_hat -eq 0 ]; then
 sudo apt-get install -y pyflakes exuberant-ctags
